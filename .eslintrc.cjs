99cea6db903a30b542fe8042b4091f332b97fe96
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-11-18 22:35:59 -0600

    chore: format files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,85 +5,78 @@ const { defineConfig } = require('eslint-define-config');
 /// <reference types='@eslint-types/typescript-eslint' />
 
 module.exports = defineConfig({
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+    '@unocss',
+    'plugin:perfectionist/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    'extends': [
-        'eslint:recommended',
-        '@nuxtjs/eslint-config-typescript',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:vue/vue3-recommended',
-        'plugin:prettier/recommended',
-        '@unocss',
-        'plugin:perfectionist/recommended'
-    ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'parser': '@typescript-eslint/parser',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        '@typescript-eslint',
-        'vue',
-        'prettier',
-        'perfectionist'
-    ],
-    'rules': {
-        'prettier/prettier': 'error',
-        'perfectionist/sort-imports': [
-            'error',
-            {
-                'type': 'natural',
-                'order': 'asc',
-                'internal-pattern': ['@/**'],
-                'newlines-between': 'always',
-                'groups': [
-                    'react',
-                    'next',
-                    'external',
-                    'external-type',
-                    'builtin',
-                    'parent',
-                    'siblings',
-                    'components',
-                    'images',
-                    'hooks',
-                    'utils',
-                    'directImport',
-                    'internal',
-                    'internal-type'
-                ],
-                'custom-groups': {
-                    'value': {
-                        'react': ['react', 'react-*'],
-                        'next': 'next/**',
-                        'images': ['@/public/**.{png,svg,jpg,jpeg,ico}'],
-                        'components': '@/components/**',
-                        'hooks': '@/lib/hooks/**',
-                        'utils': '@/utils/**',
-                        'directImport': './**'
-                    }
-                }
-            }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'vue', 'prettier', 'perfectionist'],
+  rules: {
+    'prettier/prettier': 'error',
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        type: 'natural',
+        order: 'asc',
+        'internal-pattern': ['@/**'],
+        'newlines-between': 'always',
+        groups: [
+          'react',
+          'next',
+          'external',
+          'external-type',
+          'builtin',
+          'parent',
+          'siblings',
+          'components',
+          'images',
+          'hooks',
+          'utils',
+          'directImport',
+          'internal',
+          'internal-type',
         ],
-        'import/order': 'off',
-        'sort-imports': 'off',
-        '@typescript-eslint/adjacent-overload-signatures': 'off',
-        'sort-keys': 'off'
-    }
+        'custom-groups': {
+          value: {
+            react: ['react', 'react-*'],
+            next: 'next/**',
+            images: ['@/public/**.{png,svg,jpg,jpeg,ico}'],
+            components: '@/components/**',
+            hooks: '@/lib/hooks/**',
+            utils: '@/utils/**',
+            directImport: './**',
+          },
+        },
+      },
+    ],
+    'import/order': 'off',
+    'sort-imports': 'off',
+    '@typescript-eslint/adjacent-overload-signatures': 'off',
+    'sort-keys': 'off',
+  },
 });
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,14 +1,14 @@
 // @ts-check
 
 /** @type {import("prettier").Config} */
-exports.default = {
-    semi: true,
-    singleQuote: true,
-    useTabs: false,
-    tabWidth: 2,
-    endOfLine: 'lf',
-    printWidth: 100,
-    trailingComma: 'es5',
-    proseWrap: 'preserve',
-    vueIndentScriptAndStyle: true
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  printWidth: 100,
+  trailingComma: 'es5',
+  proseWrap: 'preserve',
+  vueIndentScriptAndStyle: true,
 };
