74e361abb29caf5f1f8de09cadf37e223dd7ee6f
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-11-19 00:34:31 -0600

    chore: pretty

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,5 +3,5 @@
 /** @type {import('lint-staged').Config} */
 module.exports = {
   '*.{cjs,js,ts,vue}': 'eslint --fix',
-  '*.{cjs,js,ts,css,md}': 'prettier --write',
+  '*.{cjs,js,ts,css,md}': 'prettier --ignore-unknown --write',
 };
