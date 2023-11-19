66bf7d050161efa9e205f89bba5b531fec50ebfe
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-11-18 23:09:27 -0600

    chore: add initial nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  colorMode: {
+    classSuffix: '',
+    fallback: 'light',
+    preference: 'system',
+  },
   devtools: { enabled: true },
+  modules: ['@nuxtjs/eslint-module', '@nuxt/image', '@nuxtjs/color-mode'],
 });
