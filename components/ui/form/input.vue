9b9f653ce46700e64a8b5236aa1765253fb69ad3
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-11-24 21:25:09 -0600

    feat(auth): add Sign-Up page and Input UI component

---
+++ components/ui/form/input.vue
@@ -0,0 +1,11 @@
+<template>
+  <label>
+    <span>Input label</span>
+    <input type="text" v-bind="$attrs" />
+  </label>
+</template>
+<script setup lang="ts">
+  defineOptions({
+    inheritAttrs: false,
+  });
+</script>
---
+++ pages/auth/sign-up.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full flex gap-5">
+    <div
+      class="h-screen w-1/2 bg-[url(/auth/banner-sign-up.png)] bg-cover bg-center bg-no-repeat"
+    ></div>
+    <div class="h-screen w-1/2 flex flex-col items-center justify-center">
+      <div class="max-w-110 w-full flex flex-col gap-5">
+        <h1 class="text-5xl font-title">We're glad to have you here.</h1>
+        <p>
+          Please complete the required fields to register your user account and manage your projects
+          like never before.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
