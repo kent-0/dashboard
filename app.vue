a6fb56016878aa90782859a9e4c70992ddbc1d16
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-12-25 12:36:35 -0600

    feat(core): add loading indicator

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
 <template>
   <nuxt-layout>
+    <nuxt-loading-indicator :color="colorLoadingIndicator" />
     <nuxt-page />
     <client-only>
       <ui-overlay-notification-container />
     </client-only>
   </nuxt-layout>
 </template>
+\
+<script setup lang="ts">
+  import { theme } from '@unocss/preset-mini';
+
+  const colorLoadingIndicator = theme.colors.emerald['400'];
+</script>
