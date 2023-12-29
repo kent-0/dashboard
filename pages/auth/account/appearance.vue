7f444dab1fd3073c29f38d4baa67d20f35b8fffa
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-12-28 21:18:19 -0600

    feat(auth): ui responsive change theme

--- pages/auth/account/appearance.vue
+++ pages/auth/account/appearance.vue
@@ -12,13 +12,13 @@
       </p>
       <div
         role="button"
-        class="flex items-center rounded-md bg-components-element p-5 transition duration-300 space-x-3 dark:bg-components-elementDark hover:(ring-2 ring-brand dark:ring-brand-dark)"
+        class="flex flex-col items-center gap-3 rounded-md bg-components-element p-5 transition duration-300 md:flex-row dark:bg-components-elementDark hover:(ring-2 ring-brand dark:ring-brand-dark)"
         :class="{
           'ring-brand ring-2 dark:ring-brand-dark': theme.value === 'light',
         }"
         @click="theme.preference = 'light'"
       >
-        <div class="h-20 w-1/3 rounded-md bg-components-card"></div>
+        <div class="h-20 w-full shrink-0 rounded-md bg-components-card md:w-1/3"></div>
         <div class="flex flex-col space-y-2">
           <h2 class="text-xl">Light mode</h2>
           <p>
@@ -29,13 +29,13 @@
       </div>
       <div
         role="button"
-        class="flex items-center rounded-md bg-components-element p-5 transition duration-300 space-x-3 dark:bg-components-elementDark hover:(ring-2 ring-brand dark:ring-brand-dark)"
+        class="flex flex-col items-center gap-3 rounded-md bg-components-element p-5 transition duration-300 md:flex-row dark:bg-components-elementDark hover:(ring-2 ring-brand dark:ring-brand-dark)"
         :class="{
           'ring-brand ring-2 dark:ring-brand-dark': theme.value === 'dark',
         }"
         @click="theme.preference = 'dark'"
       >
-        <div class="h-20 w-1/3 rounded-md bg-components-cardDark"></div>
+        <div class="h-20 w-full shrink-0 rounded-md bg-components-cardDark md:w-1/3"></div>
         <div class="flex flex-col space-y-2">
           <h2 class="text-xl">Dark mode</h2>
           <p>
