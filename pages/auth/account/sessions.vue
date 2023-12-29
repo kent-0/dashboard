1028a4df75ea810048989eb6cffbf1455ef5cc62
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2023-12-28 20:53:04 -0600

    feat(auth): add advise about account sessions

--- pages/auth/account/sessions.vue
+++ pages/auth/account/sessions.vue
@@ -1,6 +1,12 @@
 <template>
   <nuxt-layout name="auth-account">
-    <h1>Sessions</h1>
+    <div class="h-96 w-full">
+      <div class="h-full flex flex-col items-center justify-center text-center">
+        <icon name="lucide:lock" class="text-primary-500" size="2rem" />
+        <h2 class="text-3xl font-bold">Sessions (soon)</h2>
+        <p>Manage your active sessions and logout from devices you don't recognize.</p>
+      </div>
+    </div>
   </nuxt-layout>
 </template>
 
